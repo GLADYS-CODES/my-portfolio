f5d8c3ebe44c6c82e46bb1977dae73cb1d0fdedd
GLADYS <mungaiglado106@gmail.com>
2023-04-28 15:17:55 +0300

    p

--- src/data/data.js
+++ src/data/data.js
@@ -1,20 +1,21 @@
-import WorkIm from '../assets/workImg.jpeg'
-import RealEst from '../assets/realestate.jpg'
 
+import RealEst from '../assets/realestate.jpg'
+import besteats from '../assets/besteats.png'
+import Ex from '../assets/Ex.png'
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"Best Eats",
+        image:besteats,
+        github:"https://github.com/GLADYS-CODES/food-app",
+        live:"https://glowing-travesseiro-d89492.netlify.app",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"Budget Tracker",
+        image:Ex,
+        github:"https://github.com/GLADYS-CODES/budget-tracker",
+        live:"https://magenta-cat-4068f4.netlify.app",
     },
     {
         id:3,
